269607f4f55ffe5c3cc3b9ac6616b2802f5b45da
S.P. Mohanty <spmohanty91@gmail.com>
2015-04-10 17:48:05 +0200

    Adding macro for simple mcq questions

--- templates/geotagx_app_logic.js
+++ templates/geotagx_app_logic.js
@@ -5,13 +5,6 @@ geotagx.task.run("{{slug}}", function(question, answer, $submitter){
         case 8: //Final Question
             geotagx.task.finish();
             break;
-        case 3:
-            if(answer=="No"){
-                geotagx.task.showQuestion(6);
-            }else{
-                geotagx.task.showNextQuestion();
-            }
-            break;
         default:
             geotagx.task.showNextQuestion();
             break;
