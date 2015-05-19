f11d812c0c2602a2f50fa21c7362a702e93245ac
Jeremy Othieno <j.othieno@gmail.com>
2015-05-19 12:19:57 +0200

    Implemented front-end analytics for projects.

--- layout/static/js/geotagx-analytics.js
+++ layout/static/js/geotagx-analytics.js
@@ -5,103 +5,134 @@
 	"use strict";
 
 	var api_ = {}; // The analytics API.
-	var projectId_ = 0; // The current project's identifier, i.e. its short_name.
 	var taskId_ = 0; // The current task's identifier.
-	var questionId_ = 0; // The current question identifier.
 
 	$(document).ready(function(){
+		window.analyticsListener = true; // Enable the CCL analytics tracker library.
+
 		// $(".external-link").on("click.analytics", onClickedExternalLink);
 		// $(".social-media-link").on("click.analytics", onSharedProject);
 
-		$(".skeleton.wet-run .btn-answer").on("click.analytics", onAnswerSubmitted);
+		$("#project-task-presenter.analysis .btn-answer").on("click.analytics", onAnswerQuestion);
 
 		//FIXME Image scrolling (zooming) events aren't generated...
-		$(".skeleton.dry-run #image").on("zoom.analytics", onTutorialImageZoom);
-		$(".skeleton.wet-run #image").on("zoom.analytics", onImageZoom);
+		$("#project-task-presenter.tutorial #image").on("zoom.analytics", onTutorialImageZoom);
+		$("#project-task-presenter.analysis #image").on("zoom.analytics", onImageZoom);
 
-		$(".skeleton.dry-run #image-source").on("click.analytics", onShowTutorialImageSource);
-		$(".skeleton.wet-run #image-source").on("click.analytics", onShowImageSource);
+		$("#project-task-presenter.tutorial #image-source").on("click.analytics", onShowTutorialImageSource);
+		$("#project-task-presenter.analysis #image-source").on("click.analytics", onShowImageSource);
 
-		$(".skeleton.dry-run #questionnaire-rewind").on("click.analytics", onShowPreviousTutorialQuestion);
-		$(".skeleton.wet-run #questionnaire-rewind").on("click.analytics", onShowPreviousQuestion);
+		$("#project-task-presenter.tutorial #questionnaire-rewind").on("click.analytics", onShowPreviousTutorialQuestion);
+		$("#project-task-presenter.analysis #questionnaire-rewind").on("click.analytics", onShowPreviousQuestion);
 
-		$(".skeleton.dry-run #questionnaire-show-comments").on("click.analytics", onShowTutorialComments);
-		$(".skeleton.wet-run #questionnaire-show-comments").on("click.analytics", onShowComments);
+		$("#project-task-presenter.tutorial #questionnaire-show-comments").on("click.analytics", onShowTutorialComments);
+		$("#project-task-presenter.analysis #questionnaire-show-comments").on("click.analytics", onShowComments);
 
-		$(".skeleton.dry-run .help-toggle").on("click.analytics", onShowTutorialHelp);
-		$(".skeleton.wet-run .help-toggle").on("click.analytics", onShowHelp);
+		$("#project-task-presenter.tutorial .help-toggle").on("click.analytics", onShowTutorialHelp);
+		$("#project-task-presenter.analysis .help-toggle").on("click.analytics", onShowHelp);
 	});
 	/**
-	 * Starts tracking analytics for the project with the specified identifier.
+	 * Starts tracking analytics for the project with the specified identifier,
+	 * and the user with the given identifier.
+	 * @param projectId the project's identifier, i.e. its short_name.
+	 * @param userId the current user's identifier, which is either a username, or an IP address for anonymous users.
 	 */
-	api_.start = function(projectId){
-		projectId_ = projectId;
-		console.log("analytics.action.startProject(" + projectId_ + ", " + window.location.href + ")");
+	api_.start = function(projectId, userId){
+		analytics.setGlobal("userId", userId ? userId : "anonymous");
+		analytics.setGlobal("projectId", projectId);
+		analytics.fireEvent("action.startProject", {"url":window.location.href});
 	}
 	/**
 	 * Updates the tracking parameters when a new task is presented to the user.
+	 * @param taskId the task's identifier.
 	 */
 	api_.onTaskChanged = function(taskId){
 		taskId_ = taskId;
 	}
 	/**
 	 * Updates the tracking parameters when a new question is presented to the user.
+	 * @param questionId the current question identifier.
 	 */
 	api_.onQuestionChanged = function(questionId){
-		questionId_ = questionId;
+		analytics.setGlobal("questionId", questionId);
 	}
-
+	/**
+	 * Fires an event when a user selects the correct answer in a tutorial.
+	 */
 	api_.onCorrectTutorialAnswer = function(){
-		console.log("analytics.action.answerCorrect(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.correctTutorialAnswer");
 	};
-
+	/**
+	 * Fires an event when a user selects the wrong answer in a tutorial.
+	 */
 	api_.onWrongTutorialAnswer = function(){
-		console.log("analytics.action.answerIncorrect(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.wrongTutorialAnswer");
 	};
-
-	function onAnswerSubmitted(){
-		console.log($(this).val());
-		console.log("analytics.action.answerProject(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+	/**
+	 * Fires an event when a user answers a question during an analysis.
+	 */
+	function onAnswerQuestion(){
+		analytics.fireEvent("action.answerQuestion", {"taskId":taskId_, "buttonValue":$(this).val()});
 	}
-
+	/**
+	 * Fires an event when a user zooms in on an image during a tutorial.
+	 */
 	function onTutorialImageZoom(){
-		console.log("analytics.action.photoZoomTut(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.tutorialImageZoom");
 	}
-
+	/**
+	 * Fires an event when a user zooms in on an image during an analysis.
+	 */
 	function onImageZoom(){
-		console.log("analytics.action.photoZoom(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.imageZoom", {"taskId":taskId_});
 	}
-
+	/**
+	 * Fires an event when a user visits an image's source during a tutorial.
+	 */
 	function onShowTutorialImageSource(){
-		console.log("analytics.action.viewPhotoSourceTut(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showTutorialImageSource");
 	}
-
+	/**
+	 * Fires an event when a user visits an image's source during an analysis.
+	 */
 	function onShowImageSource(){
-		console.log("analytics.action.viewPhotoSource(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showImageSource", {"taskId":taskId_});
 	}
-
+	/**
+	 * Fires an event when a user goes back to a previous question during a tutorial.
+	 */
 	function onShowPreviousTutorialQuestion(){
-		console.log("analytics.action.previousQuestionTut(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showPreviousTutorialQuestion");
 	}
-
+	/**
+	 * Fires an event when a user goes back to a previous question during an analysis.
+	 */
 	function onShowPreviousQuestion(){
-		console.log("analytics.action.previousQuestion(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showPreviousQuestion", {"taskId":taskId_});
 	}
-
+	/**
+	 * Fires an event when a user clicks the 'Show Comments' button during a tutorial.
+	 */
 	function onShowTutorialComments(){
-		console.log("analytics.action.showCommentsTut(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showTutorialComments");
 	}
-
+	/**
+	 * Fires an event when a user clicks the 'Show Comments' button during an analysis.
+	 */
 	function onShowComments(){
-		console.log("analytics.action.showComments(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showComments", {"taskId":taskId_});
 	}
-
+	/**
+	 * Fires an event when a user clicks a question's help toggle during a tutorial.
+	 */
 	function onShowTutorialHelp(){
-		console.log("analytics.action.helpRequest(" + projectId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showTutorialHelp");
 	}
-
+	/**
+	 * Fires an event when a user clicks a question's help toggle during an analysis.
+	 */
 	function onShowHelp(){
-		console.log("analytics.action.helpRequest(" + projectId_ + ", " + taskId_ + ", " + questionId_ + ")");
+		analytics.fireEvent("action.showHelp", {"taskId":taskId_});
 	}
 
 	// Expose the API.
