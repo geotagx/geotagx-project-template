8200142e73723ae474fe0e17309b4b32779b4b54
Jeremy Othieno <j.othieno@gmail.com>
2015-09-02 15:56:54 +0200

    Add unit test stubs

---
+++ src/test_tutorial.py
@@ -0,0 +1,24 @@
+# This module is part of the GeoTag-X project builder.
+# Copyright (C) 2015 UNITAR.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import unittest
+from tutorial import Tutorial
+
+class TestTutorial(unittest.TestCase):
+	pass
+
+
+if __name__ == "__main__":
+	unittest.main()
