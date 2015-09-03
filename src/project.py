b5495786b9dda49aa2d75102fb4659f4cf463bc6
Jeremy Othieno <j.othieno@gmail.com>
2015-09-04 01:12:50 +0200

    Remove an erroneous import

--- src/project.py
+++ src/project.py
@@ -15,7 +15,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 import os
 from src.questionnaire import Questionnaire
-from src.tutorial      import Tutorial
 
 class Project:
 	path = None
