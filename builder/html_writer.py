e8535a4bd4294d2b3d0d1bec5122c271f0b2d2ab
Jeremy Othieno <j.othieno@gmail.com>
2016-03-22 17:38:44 +0100

    Implement the HTML writer

--- builder/html_writer.py
+++ builder/html_writer.py
@@ -1,6 +1,6 @@
 # The GeoTag-X project builder tool.
 # This module contains elements that will help write a project's task presenter
-# (task_presenter.html) and tutorial (tutorial.html).
+# (template.html) and tutorial (tutorial.html).
 #
 # Copyright (c) 2016 UNITAR/UNOSAT
 #
@@ -22,10 +22,94 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
+import geotagx_sanitizer.sanitizer as sanitizer
+
 class HtmlWriter():
-	def __init__(self, theme_path):
-		self.theme_path = theme_path
+	def __init__(self):
+		self.sanitizers = sanitizer.get_sanitizers()
 
 
-	def write(self, path, overwrite=False, compress=False, verbose=False):
-		pass
+	def _get_configurations(self, path):
+		"""Returns the sanitized project configurations located at the specified path."""
+		import os
+		from geotagx_sanitizer.utils import read_json
+
+		filenames = {k: os.path.join(path, k + ".json") for k in self.sanitizers}
+		configurations = {k: read_json(filename) for k, filename in filenames.iteritems()}
+
+		# Add help to the configurations.
+		questions = configurations["task_presenter"]["questionnaire"]["questions"]
+		for question in questions:
+			key = question["key"]
+			try:
+				help_file = open(os.path.join(path, "help", key + ".html"))
+			except IOError:
+				# A help file is not always guaranteed to exist so if an IOError occurs, skip it.
+				pass
+			else:
+				with help_file:
+					from htmlmin import minify
+					question["help"] = minify(help_file.read(), remove_comments=True, remove_empty_space=True)
+
+		return sanitizer.sanitize(configurations, self.sanitizers)
+
+
+	@staticmethod
+	def timestamp():
+		import datetime
+		return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S+00:00")
+
+
+	@staticmethod
+	def jsonify(input, compress=False):
+		import json
+		parameters = {
+			"sort_keys": not compress,
+			"indent": 0 if compress else 4,
+			"separators": (",", ":" if compress else ": "),
+			"encoding": "UTF-8",
+			"ensure_ascii": False
+		}
+		output = json.dumps(input, **parameters).encode("UTF-8")
+		return output.replace("\n", "") if compress else output
+
+
+	#TODO Make this write(self, configurations, overwrite)
+	def write(self, path, overwrite=False, compress=False):
+		import os, logging
+		from exceptions import HtmlWriterError
+
+		if not os.path.isdir(path):
+			raise HtmlWriterError("The path '%s' does not point to a directory." % path)
+		elif not os.access(path, os.W_OK):
+			raise HtmlWriterError("The directory '%s' is not writable. Please make sure you have the appropriate access permissions." % path)
+
+		# If the overwrite flag is not set, make sure none of the target HTML files do not exist.
+		TARGET_FILES = {
+			"task_presenter": os.path.join(path, "template.html"),
+			"tutorial": os.path.join(path, "tutorial.html")
+		}
+		if not overwrite and any(os.path.isfile(f) for f in TARGET_FILES.values()):
+			raise HtmlWriterError("The directory '%s' already contains a generated task presenter (template.html) and or a tutorial (tutorial.html). To overwrite either, set the '-f' or '--force' flag." % path)
+
+		configurations = self._get_configurations(path)
+
+		# Remove the fields that can be retrieved from the server's database.
+		project = configurations["project"]
+		project["last_updated"] = HtmlWriter.timestamp()
+		for key in ["name", "short_name", "description"]:
+			project.pop(key, None)
+
+		# Write the project's tutorial. Note that if no tutorial configuration exists,
+		# an empty 'tutorial.html' file is still created as it is a requirement for
+		# PyBossa's 'pbs' tool.
+		with open(TARGET_FILES["tutorial"], "w") as file:
+			if "tutorial" in configurations:
+				logging.info("Writing tutorial (%s) ..." % TARGET_FILES["tutorial"])
+				file.write(HtmlWriter.jsonify(configurations, compress))
+
+		# Write the task presenter.
+		with open(TARGET_FILES["task_presenter"], "w") as file:
+			configurations.pop("tutorial", None) # Remove the tutorial configuration, if it exists.
+			logging.info("Writing task presenter (%s) ..." % TARGET_FILES["task_presenter"])
+			file.write(HtmlWriter.jsonify(configurations, compress))
