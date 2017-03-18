f4ff2fe3a96afd5cbafc6f497428334f826bb04e
Jeremy Othieno <j.othieno@gmail.com>
2017-03-18 16:24:27 +0100

    Implement the get_formatted_configuration_set function

--- src/helper.py
+++ src/helper.py
@@ -28,7 +28,29 @@
 from geotagx_validator.helper import check_arg_type
 
 def get_formatted_configuration_set(path, enable_logging=False):
-    raise NotImplementedError
+    """Returns a set of formatted configurations for the GeoTag-X project
+    located at the specified path.
+
+    Args:
+        path (str): A path to a directory containing a GeoTag-X project.
+        enable_logging (bool): If set to True, the function will log most of its operations.
+
+    Returns:
+        dict|None: A dictionary containing a set of formatted configurations if the specified
+            path contains a valid GeoTag-X project, None otherwise.
+
+    Raises:
+        TypeError: If the path argument is not a string or enable_logging is not a boolean.
+        IOError: If the specified path is inaccessible or not a directory, or if a required
+            configuration in the directory at the specified path is inaccessible.
+    """
+    check_arg_type(get_formatted_configuration_set, "path", path, basestring)
+    check_arg_type(get_formatted_configuration_set, "enable_logging", enable_logging, bool)
+
+    from geotagx_validator.helper import deserialize_configuration_set
+    from geotagx_formatter.core import format_configuration_set
+
+    return format_configuration_set(deserialize_configuration_set(path), enable_logging)
 
 
 def serialize_configuration_set(configuration_set, path, overwrite=False, compress=False):
